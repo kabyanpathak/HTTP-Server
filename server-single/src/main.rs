cf12cbba57dc5f12b966801036543fb64176cd71
kabyanpathak <kabyan388@gmail.com>
2025-05-13 13:52:32 -0700

    actually reading requests

--- server-single/src/main.rs
+++ server-single/src/main.rs
@@ -1,11 +1,25 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
+    }
+
+    fn handle_connection(stream: TcpStream) {
+        let buf_reader = BufReader::new(&stream);
+        let http: Vec<_> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+
+        println!("Request: {http:#?}");
     }
 }
