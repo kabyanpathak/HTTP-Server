1abdf3189d83e2c8b957e9d3bdd449a5bdc0b44f
kabyanpathak <kabyan388@gmail.com>
2025-05-15 15:18:13 -0700

    some refactoring

--- server-single/src/main.rs
+++ server-single/src/main.rs
@@ -16,19 +16,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request = buf_reader.lines().next().unwrap().unwrap();
 
-    let status = "HTTP/1.1 200 OK\r\n\r\n";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    if request == "GET / HTTP/1.1" {
+        let status = "HTTP/1.1 200 OK\r\n\r\n";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
-
-    print!("{http:#?}");
+        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+    }
 }
