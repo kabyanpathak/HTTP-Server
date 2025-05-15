8c7fad7c9577d8dbf445925179b4a76cbfb6044c
kabyanpathak <kabyan388@gmail.com>
2025-05-15 16:57:26 -0700

    finished single threaded

--- server-single/src/main.rs
+++ server-single/src/main.rs
@@ -18,13 +18,16 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request = buf_reader.lines().next().unwrap().unwrap();
 
-    if request == "GET / HTTP/1.1" {
-        let status = "HTTP/1.1 200 OK\r\n\r\n";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status, filename) = if request == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-    }
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
