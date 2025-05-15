ff4917a23124cf911aa609279a21c2e3c9f6d14c
kabyanpathak <kabyan388@gmail.com>
2025-05-15 15:10:45 -0700

    fixed indentation

--- server-single/src/main.rs
+++ server-single/src/main.rs
@@ -12,23 +12,23 @@ fn main() {
 
         handle_connection(stream);
     }
+}
 
-    fn handle_connection(mut stream: TcpStream) {
-        let buf_reader = BufReader::new(&stream);
-        let http: Vec<_> = buf_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-        let status = "HTTP/1.1 200 OK\r\n\r\n";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    let status = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
 
-        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-        stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
 
-        print!("{http:#?}");
-    }
+    print!("{http:#?}");
 }
